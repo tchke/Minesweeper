833cce3a07b7aa6f5d3af8a516827b9598bcc0ff
tcho thcke <tchk953@gmail.com>
2018-02-22 12:50:26 +0200

    Added times to repeat

--- src/Repeat.js
+++ src/Repeat.js
@@ -3,8 +3,9 @@ import React from 'react';
 export default class Repeat extends React.Component {
     render() {
         const items = [];
+        const { times = 10 } = this.props;
 
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < times; i++) {
             items.push(this.props.children(i));
         }
         
