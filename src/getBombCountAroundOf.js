6d60373530287bf165ddbf30b7141ec4562c0a53
tcho thcke <tchk953@gmail.com>
2018-03-13 16:08:36 +0200

    implementation bomb counting

--- src/getBombCountAroundOf.js
+++ src/getBombCountAroundOf.js
@@ -1,8 +1,23 @@
 import bombMap from './bombMap';
+import hasBomb from './hasBomb';
 
 function createGetBombCountAroundOf() {
+    const checkPos = (i,j) => {
+        return i >= 0 && j >= 0 && hasBomb(i, j);
+    }
+    
     return function(i, j) {
-        return 3;
+        let count = 0;
+        for (let k = i - 1; k <= i + 1; k++) {
+            console.log(`k is ${k}`);
+            for (let z = j - 1; z <= j + 1; z++) {
+                console.log(`z is ${z}`);
+                if (checkPos(k,z)) {                    
+                    count++;
+                }
+            }
+        } 
+        return count;       
     }
 }
 
