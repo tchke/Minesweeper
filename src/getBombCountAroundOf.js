cab87efa3d35ac8ec7b7dfd4c4b53b98a7fdd82e
tcho thcke <tchk953@gmail.com>
2018-03-13 16:57:43 +0200

    changed searching bounds

--- src/getBombCountAroundOf.js
+++ src/getBombCountAroundOf.js
@@ -3,15 +3,15 @@ import hasBomb from './hasBomb';
 
 function createGetBombCountAroundOf() {
     const checkPos = (i,j) => {
-        return i >= 0 && j >= 0 && hasBomb(i, j);
+        return i >= 0 && i < 10 && j >= 0 && j < 10 && hasBomb(i, j);
     }
     
     return function(i, j) {
         let count = 0;
         for (let k = i - 1; k <= i + 1; k++) {
-            console.log(`k is ${k}`);
+            //console.log(`k is ${k}`);
             for (let z = j - 1; z <= j + 1; z++) {
-                console.log(`z is ${z}`);
+                //console.log(`z is ${z}`);
                 if (checkPos(k,z)) {                    
                     count++;
                 }
